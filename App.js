91d8c05b72ce2063572f61abbce63b00433ea6f7
piyooshgu12 <126654300+piyooshgu12@users.noreply.github.com>
2023-03-07 11:06:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1 align="left"> i am heading one ...from first component</h1>
+      <h2 align="left">i am heading two...from second component</h2>
+      <h3 align="left">I am heading three ... from third component</h3>
+      <button id="btn"> click me!!!</button> 
+
+    </div>
+  );
+}
+
+export default App;
