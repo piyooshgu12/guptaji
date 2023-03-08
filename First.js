6196ed98a9e6b41bf721cea050941eaa6cd0a514
piyooshgu12 <126654300+piyooshgu12@users.noreply.github.com>
2023-03-08 21:13:49 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export 
+ function First() {
+
+return(
+<form class="container">
+    <div class="container">
+        <button> mobile...</button> <br></br>
+        <button>password</button> <br></br>
+        <button> submit</button> <br></br> 
+
+    </div>
+
+    
+
+
+    </form>
+)
+
+
+}
+export default First
